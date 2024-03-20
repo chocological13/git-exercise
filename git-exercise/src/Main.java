464f47874ebe2043915ee5cf1824ab1f659e92ed
nina <just@practice.com>
2024-03-20 15:14:29 +0700

    feat: a commit on the featureA branch

--- git-exercise/src/Main.java
+++ git-exercise/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
         System.out.println("This is a test for git exercise.");
+        System.out.println("This is to test having branches.");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
